cccad203d92fe29bb81141f098bfd0cd0508eb3a
Samysr17 <108344579+Samysr17@users.noreply.github.com>
2023-07-05 23:59:42 +0530

    Readdir

--- Dir_man_sys_calls/ReadDir.c
+++ Dir_man_sys_calls/ReadDir.c
@@ -1,4 +1,19 @@
-#include<stdio.h>
-int main(){
-    return 0;
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+struct dirent *dptr;
+int main()
+{
+  char buff[100];
+  DIR *dir_pointer;
+  printf("Enter directory name");
+  scanf("%s",buff);
+  if(dir_pointer==opendir(buff)==NULL){
+    printf("Directory doesn't exist");
+  }
+  while(dptr==readdir(dir_pointer)){
+    printf("%s",dptr->d_name);
+  }
+  closedir(dir_pointer);
+   return 0;
 }
